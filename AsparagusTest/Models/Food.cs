22d834a5f6d4cd21a93bae8db7c25e5dd4b5d933
MrNesta <pavlovsergey1991@gmail.com>
2016-04-11 15:32:21 +0300

    Add project

---
+++ AsparagusTest/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsparagusTest.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public DateTime EatingDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
