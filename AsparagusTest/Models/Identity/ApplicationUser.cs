beb8ad47f90a51e4e4400718df6e744b08f4741f
MrNesta <pavlovsergey1991@gmail.com>
2016-04-17 13:18:06 +0300

    External login

--- AsparagusTest/Models/Identity/ApplicationUser.cs
+++ AsparagusTest/Models/Identity/ApplicationUser.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AsparagusTest.Models.Identity
 {
@@ -11,6 +14,14 @@ namespace AsparagusTest.Models.Identity
         public ApplicationUser()
         {
 
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(ApplicationUserManager applicationUserManager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await applicationUserManager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
         }
     }
 }
